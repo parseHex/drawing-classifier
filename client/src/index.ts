9db2f5dc917f000ee693935afe72a85a8c5721a8
Thomas Mays <thomas@mindofthomas.com>
2018-05-02 21:55:26 -0400

    Refactor main script

--- client/src/index.ts
+++ client/src/index.ts
@@ -2,17 +2,23 @@ import * as c from '../../../canvas/src';
 import * as nn from './nn';
 import * as utils from './train/utils';
 
-c.setCanvas(<HTMLCanvasElement>document.getElementById('main'));
-
 const guess = <HTMLSpanElement>document.getElementById('guess');
-const canvas = <HTMLCanvasElement>document.getElementById('helper');
-const ctx = canvas.getContext('2d');
+const reset = <HTMLButtonElement>document.getElementById('reset');
 
-const cx = c.getContext();
+const mainCanvas = <HTMLCanvasElement>document.getElementById('main');
+const helperCanvas = <HTMLCanvasElement>document.getElementById('helper');
+const helperCtx = helperCanvas.getContext('2d');
+const mainCtx = mainCanvas.getContext('2d');
+
+let changed = true;
+
+c.setCanvas(mainCanvas);
 
 function drawLoop() {
-	ctx.drawImage(cx.canvas, 0, 0, 28, 28);
-	const img = ctx.getImageData(0, 0, 28, 28);
+	if (!changed) return;
+
+	helperCtx.drawImage(mainCtx.canvas, 0, 0, 28, 28);
+	const img = helperCtx.getImageData(0, 0, 28, 28);
 	let px = [];
 	for (let i = 0; i < img.data.length; i += 4) {
 		px.push(utils.map(img.data[i], 0, 255, 0, 1));
@@ -29,9 +35,17 @@ c.setLoop({
 });
 
 c.utility.onDrag(function (position) {
+	changed = true;
+
 	c.draw.circle({
 		x: position.x,
 		y: position.y,
 		rx: 10,
 	});
+});
+
+reset.addEventListener('click', function () {
+	changed = true;
+	mainCtx.fillStyle = 'black';
+	mainCtx.fillRect(0, 0, mainCanvas.width, mainCanvas.height);
 });
